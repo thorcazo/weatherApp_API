9da09e392aef0654f69528e4bc9276fc44c0b0c0
thorcazo <thorcazodesign@gmail.com>
2024-01-30 21:01:41 +0100

    mejorando animacion menu

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -8,9 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
-  clickToggle = true;
+  clickToggle = false;
 
   clicked() {
     this.clickToggle = !this.clickToggle;
+    console.log(this.clickToggle);
   }
 }
