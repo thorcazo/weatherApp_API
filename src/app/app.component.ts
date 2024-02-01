08818d30a019af8d99c68b90815854ea3317caa2
thorcazo <thorcazodesign@gmail.com>
2024-02-01 18:05:33 +0100

    limpiadas de imports y variables innecesarias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
-
-import { TiempoComponent } from './components/tiempo/tiempo.component';
 import { NavComponent } from './components/nav/nav.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 
@@ -13,7 +11,6 @@ import { InicioComponent } from './components/inicio/inicio.component';
     AsyncPipe,
     CommonModule,
     RouterOutlet,
-    TiempoComponent,
     NavComponent,
     RouterLink,
     InicioComponent,
@@ -21,68 +18,4 @@ import { InicioComponent } from './components/inicio/inicio.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-
-//APP COMPONENT
-export class AppComponent {
-  // http = inject(HttpClient);
-  // service = inject(OpenWeatherService);
-  // //arrays donde se almacenaran los datos
-  // tiempo: any = [];
-  // tiempoPronostico: any = [];
-  // //busqueda predeterminada
-  // busqueda = this.mostrarDatos('Murcia');
-  // mostrarDatos(ciudad: string) {
-  //   return this.fetchtiempo(ciudad);
-  // }
-  // fetchtiempo(ciudad: string): Observable<any> {
-  //   return this.service.getOpenWeather(ciudad).pipe(
-  //     tap((res) => {
-  //       this.tiempo = res;
-  //     })
-  //   );
-  // }
-  // getForecast(ciudad: string, lat: number, lon: number): Observable<any> {
-  //   return this.service.getForecast(ciudad, lat, lon);
-  // }
-  // mostrartiempoPronostico(ciudad: string) {
-  //   this.fetchtiempo(ciudad).subscribe((datos) => {
-  //     this.getForecast(ciudad, datos.coord.lat, datos.coord.lon).subscribe(
-  //       (forecast) => {
-  //         console.log(datos);
-  //         console.log(forecast);
-  //         let diaActual = new Date();
-  //         this.tiempo = {
-  //           ciudad: datos.name,
-  //           dia: diaActual.toLocaleDateString('es-ES', {
-  //             weekday: 'long',
-  //             year: 'numeric',
-  //             month: 'long',
-  //             day: 'numeric',
-  //           }),
-  //           pais: datos.sys.country,
-  //           temperatura: datos.main.temp,
-  //           max: datos.main.temp_max,
-  //           min: datos.main.temp_min,
-  //           hora: this.convertirTimestampAHorasMinutos(datos.dt),
-  //           descripcion: datos.weather[0].description,
-  //           viento: datos.wind.speed,
-  //           humedad: datos.main.humidity,
-  //         };
-  //         console.log(this.tiempo);
-  //         this.tiempoPronostico = forecast;
-  //       }
-  //     );
-  //   });
-  // }
-  // convertirTimestampAHorasMinutos(timestamp: number) {
-  //   // Verificar si el timestamp es un número válido
-  //   if (!isNaN(timestamp) && Number.isFinite(timestamp)) {
-  //     var fecha = new Date(timestamp * 1000);
-  //     var horas = fecha.getHours();
-  //     var minutos = '0' + fecha.getMinutes();
-  //     return horas + ':' + minutos.substr(-2);
-  //   } else {
-  //     return '';
-  //   }
-  // }
-}
+export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { routes } from './app.routes';
-import { routesUser } from './components/user-profile/user-profile.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
