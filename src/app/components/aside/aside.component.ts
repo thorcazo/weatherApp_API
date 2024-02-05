f3093c7b3cbdabd0474a8721ee788cc4e9fe63eb
thorcazo <thorcazodesign@gmail.com>
2024-02-05 10:16:46 +0100

    Update styles.css, app.component.html, inicio.component.css, and aside.component.ts and .html files

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-aside',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './aside.component.html',
-  styleUrl: './aside.component.css'
+  styleUrl: './aside.component.css',
 })
-export class AsideComponent {
-
-}
+export class AsideComponent {}
