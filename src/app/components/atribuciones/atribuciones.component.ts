bbd9d9379fa489b9016cf46ffe74535c89a442dc
David Pastor <45442766+thorcazo@users.noreply.github.com>
2024-02-12 11:57:03 +0100

    Update styles.css and nav.component.html, add AtribucionesComponent to app.routes.ts, and modify inicio.component.html
    cambios en styles.css y navComponent para poner las atribuciones y su ruta

---
+++ src/app/components/atribuciones/atribuciones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atribuciones',
+  standalone: true,
+  imports: [],
+  templateUrl: './atribuciones.component.html',
+  styleUrl: './atribuciones.component.css'
+})
+export class AtribucionesComponent {
+
+}
