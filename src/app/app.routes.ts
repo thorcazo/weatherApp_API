f4c7dd0b27b74bff07e99703a56d47c76fd448f7
David Pastor <45442766+thorcazo@users.noreply.github.com>
2024-02-13 19:59:58 +0100

    Add podcast component and error404 component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { PublicacionDetailsComponent } from './components/publicacion-details/publicacion-details.component';
 import { AtribucionesComponent } from './components/atribuciones/atribuciones.component';
 import { PublicacionesUsuariosComponent } from './components/publicaciones-usuarios/publicaciones-usuarios.component';
+import { PodcastComponent } from './components/podcast/podcast.component';
+import { Error404Component } from './components/error404/error404.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
@@ -11,5 +13,7 @@ export const routes: Routes = [
   { path: 'user/:userId/publicacion/:pubId', component: PublicacionDetailsComponent },
   { path: 'user/:userId/publicacion/:pubId/user/:UserId', redirectTo: 'user/:UserId' },
   { path: 'atribuciones', component: AtribucionesComponent},
-  {path: 'publicaciones-usuarios', component: PublicacionesUsuariosComponent}
+  {path: 'podcast', component: PodcastComponent},
+  {path: 'publicaciones-usuarios', component: PublicacionesUsuariosComponent},
+  { path: '**', component: Error404Component }
 ];
