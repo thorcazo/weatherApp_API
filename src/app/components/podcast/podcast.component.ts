a82dfd8f9f87c7c7d56c69ab66a9c7b1d01416fb
David Pastor <45442766+thorcazo@users.noreply.github.com>
2024-02-14 08:23:45 +0100

    Refactor podcast component: Add styling and functionality

--- src/app/components/podcast/podcast.component.ts
+++ src/app/components/podcast/podcast.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-podcast',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './podcast.component.html',
   styleUrl: './podcast.component.css'
 })
 export class PodcastComponent {
+
+  activateText = false;
+
+  constructor() { }
+
+  toggleText() {
+    this.activateText = !this.activateText;
+  }
 
 }
